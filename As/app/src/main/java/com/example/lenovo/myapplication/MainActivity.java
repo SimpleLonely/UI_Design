a2e75ce60d181953f5a9330ad9fc4b90c875da53
SimpleLonely <32236576+SimpleLonely@users.noreply.github.com>
2018-02-01 21:31:31 +0800

    主界面+相机界面
    现在只有那个相机按钮是能点的，其他按钮点了只会变色~
    不知为什么在as的界面这些图虽然丑但是位置好歹是对的，用模拟器一跑位置就全变了.....

--- As/app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ As/app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -5,13 +5,17 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    /*private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
@@ -29,14 +33,21 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         }
-    };
+    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextMessage = (TextView) findViewById(R.id.message);
-    }
+        Button camera_button=(Button)this.findViewById(R.id.camera_button);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_camera);
+            }
+        });
+    };
+
 
 }
