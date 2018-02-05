6e10dcff9a21edaf41bcad5a82a23a55e28b6f67
SimpleLonely <32236576+SimpleLonely@users.noreply.github.com>
2018-02-05 22:12:06 +0800

    修复了一个小bug

--- As/app/src/main/java/com/example/lenovo/myapplication/FeedbackActivity.java
+++ As/app/src/main/java/com/example/lenovo/myapplication/FeedbackActivity.java
@@ -11,7 +11,6 @@ public class FeedbackActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feedback);
     }
 
 }
